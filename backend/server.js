11df1b72b6cee9387ebd57282d04d877d3a0bed8
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2023-11-08 21:09:08 -0800

    server file

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const patientRoutes = require('./routes/patients.js');
+const adminRoutes = require('./routes/admins');
+const doctorRoutes = require('./routes/doctors');
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello my good friend!');
+});
+
+app.use('/patients', patientRoutes);
+app.use('/admins', adminRoutes);
+app.use('/doctors', doctorRoutes);
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log('Connected!');
+    app.listen(process.env.PORT, () => {
+      console.log(`Node API app is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
