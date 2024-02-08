22414b0b34675b1bf8dbc060d4396cf7070507fa
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2024-02-07 22:54:48 -0800

    services and user stories sections

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+/*images*/
 import first from './imgs/doctors-patient.jpg';
 import second from './imgs/data_storage.jpg';
 import third from './imgs/wait-room.jpg';
@@ -8,9 +13,12 @@ import sixth from './imgs/chido.jpg';
 import seven from './imgs/nc.jpg';
 import eight from './imgs/nr.jpg';
 import nine from './imgs/dr.jpg';
-import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import ten from './imgs/records.jpg';
+import elle from './imgs/appointments.jpg';
+import twelve from './imgs/payment.jpg';
+import thirteen from './imgs/doc.jpg';
+import fourteen from './imgs/healthcare-worker.jpg';
+import fifteen from './imgs/patient.jpg';
 
 function Landing() {
   return (
@@ -96,7 +104,7 @@ function Landing() {
                 <Card.Title className="team-member-name">
                   Nicklas Chang
                 </Card.Title>
-                <Card.Text>Group Liaison</Card.Text>
+                <Card.Text>Group Liaison | Front-end Development</Card.Text>
               </Card.Body>
             </Card>
             <Card className="team-member-card">
@@ -105,7 +113,7 @@ function Landing() {
                 <Card.Title className="team-member-name">
                   Chidi Okpara
                 </Card.Title>
-                <Card.Text>Full Stack Developer</Card.Text>
+                <Card.Text>Full Stack Development</Card.Text>
               </Card.Body>
             </Card>
             <Card className="team-member-card">
@@ -129,16 +137,63 @@ function Landing() {
           </div>
         </div>
         <div className="runner-service container-fluid">
-          <div className="runner-service-l">
-            <h1 className="subsec-title">Services</h1>
+          <h1 className="subsec-title">Services</h1>
+          <div className="all-services-can">
+            <div className="services-can">
+              <div className="service-img-can">
+                <img className="service-img" alt=".." src={ten} />
+              </div>
+              <div className="service-desc">Store Medical Records</div>
+            </div>
+            <div className="service-divider"></div>
+            <div className="services-can">
+              <div className="service-img-can">
+                <img className="service-img" alt=".." src={elle} />
+              </div>
+              <div className="service-desc">Schedule Appointments</div>
+            </div>
+            <div className="service-divider"></div>
+            <div className="services-can">
+              <div className="service-img-can">
+                <img className="service-img" alt=".." src={twelve} />
+              </div>
+              <div className="service-desc">Make Payments</div>
+            </div>
           </div>
-          <div className="runner-service-r"></div>
         </div>
         <div className="runner-review container-fluid">
-          <div className="runner-review-l">
-            <h1 className="subsec-title">User Stories</h1>
+          <h1 className="subsec-title">User Stories</h1>
+          <div className="all-cases-can">
+            <div className="cases-can">
+              <div className="case-img-can">
+                <img className="case-img" alt=".." src={thirteen} />
+              </div>
+              <div className="case-desc">
+                "As a physician, I need access to a patient's medical history so
+                I can provide the best care possible."
+              </div>
+            </div>
+            <div className="case-divider"></div>
+            <div className="cases-can">
+              <div className="case-img-can">
+                <img className="case-img" alt=".." src={fourteen} />
+              </div>
+              <div className="case-desc">
+                "As a healthcare worker, I want a healthcare management system
+                that will help schedule appointments and manage payments."
+              </div>
+            </div>
+            <div className="case-divider"></div>
+            <div className="cases-can">
+              <div className="case-img-can">
+                <img className="case-img" alt=".." src={fifteen} />
+              </div>
+              <div className="case-desc">
+                "As a patient, I want to be able to store my health records in
+                one place. That way, I won't worry about losing them."
+              </div>
+            </div>
           </div>
-          <div className="runner-review-r"></div>
         </div>
         <div className="runner-contact container-fluid">
           <div className="runner-contact-l">
