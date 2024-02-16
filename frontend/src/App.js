69b61b96ea58a3026a07ef2496bb47f106c20a5e
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2024-02-16 03:50:52 -0800

    scroll to top when clicking on a link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
 import { useMemo } from 'react';
 import { IconContext } from 'react-icons/lib';
+import ToTop from './helpers/ScrollToTop.jsx';
 
 //pages and components
 import PatientHome from './pages/PatientHome';
 import PatientLogin from './pages/PatientLogin';
-import AppointmentSelector from './pages/AppointmentSelector.js'
+import AppointmentSelector from './pages/AppointmentSelector.js';
 import Landing from './pages/Landing';
 import PatientSignup from './pages/PatientSignup';
 import Navi from './components/navbar/Navi';
@@ -16,7 +17,6 @@ import BookAppointments from './pages/BookAppointment.js';
 import AppointmentConfirmation from './pages/AppointmentConfirmation.js';
 import AppointmentDashboard from './pages/AppointmentDashboard.js';
 
-
 function App() {
   const { patient } = useAuthContext();
   const foo = useMemo(() => ({ size: '2em' }), []);
@@ -25,6 +25,7 @@ function App() {
     <IconContext.Provider value={foo}>
       <div className="App">
         <BrowserRouter>
+          <ToTop />
           <Navi />
           <div className="pages">
             <Routes>
@@ -51,28 +52,43 @@ function App() {
               <Route
                 path="/appointment"
                 element={
-                patient ? <AppointmentSelector /> : <Navigate to="/patient_login" />
-                 }
+                  patient ? (
+                    <AppointmentSelector />
+                  ) : (
+                    <Navigate to="/patient_login" />
+                  )
+                }
               />
               <Route
                 path="/appointment/:id"
                 element={
-                patient ? <BookAppointments /> : <Navigate to="/patient_login" />
-                 }
+                  patient ? (
+                    <BookAppointments />
+                  ) : (
+                    <Navigate to="/patient_login" />
+                  )
+                }
               />
               <Route
                 path="/appointment/:id/:id"
                 element={
-                patient ? <AppointmentConfirmation /> : <Navigate to="/patient_login" />
-                 }
+                  patient ? (
+                    <AppointmentConfirmation />
+                  ) : (
+                    <Navigate to="/patient_login" />
+                  )
+                }
               />
               <Route
                 path="/appointment/dashboard/" // NOTE: Consider adding an id paramter which will limit view
                 element={
-                patient ? <AppointmentDashboard /> : <Navigate to="/patient_login" />
-                 }
+                  patient ? (
+                    <AppointmentDashboard />
+                  ) : (
+                    <Navigate to="/patient_login" />
+                  )
+                }
               />
-              
             </Routes>
             <Footer />
             <MNavi />
@@ -84,4 +100,3 @@ function App() {
 }
 
 export default App;
-
