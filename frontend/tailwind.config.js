a0031a26fea3a4cc822e550a6be9ec468b3ed973
Nicklas Chiang <nicklasmc1@gmail.com>
2024-02-24 14:12:14 -0800

    Additions to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'goldenrod': '#daa520 ',
+        'runnerblue': '#061237',
+      },
+      screens: {
+        'mobile': {'max': '800px'},
+        'medium': {'max': '1151px'},
+      },
+    },
   },
   plugins: [],
 };
