dfedb013248badd8010b6a66500f4e519cfa3c21
Nicklas Chiang <nicklasmc1@gmail.com>
2024-02-28 11:48:59 -0800

    Navbar Styles and Dropdown

--- frontend/src/components/navbar/Navi.js
+++ frontend/src/components/navbar/Navi.js
@@ -3,16 +3,20 @@ import { UseLogout } from '../../hooks/useLogout';
 import { UseAdminLogout } from '../../hooks/useAdminLogout';
 import { UseDoctorLogout } from '../../hooks/useDoctorLogout';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import Logo from '../imgs/rh_logo_shadow.png';
+import Logo from '../../pages/imgs/rh_logo_shadow.png';
+import first from'../../pages/imgs/placeholder.png';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
+import { VscAccount } from 'react-icons/vsc';
 
 const Navi = () => {
   const { logout } = UseLogout();
   const { adminLogout } = UseAdminLogout();
   const { doctorLogout } = UseDoctorLogout();
   const { patient, admin, doctor } = useAuthContext();
+  const navDropdownTitle = (<span className="bg-gray"><VscAccount /></span>);
 
   const handleClick = () => {
     logout();
@@ -27,7 +31,7 @@ const Navi = () => {
   };
 
   return (
-    <Navbar className="bg-body-tertiary" sticky="top">
+    <Navbar className="bg-body-tertiary" fixed="top">
       <Container fluid className="topnav-contents">
         <Navbar.Brand className="rh-topnav-logo">
           <Link to="/">
@@ -54,17 +58,19 @@ const Navi = () => {
               <Link to="/invoice" className="link-btn">
                 Invoice
               </Link>
-              <div>
-                <span style={{ padding: 10 }} className="pname">
-                  {patient.email}
-                </span>
-                <Button
-                  onClick={handleClick}
-                  className="btn logout-btn bg-slate-500"
-                  variant="secondary"
-                >
-                  LOGOUT
-                </Button>
+              <div className="flex justify-center items-center">
+                <NavDropdown className="flex flex-col justify-center items-center pl-5" title={navDropdownTitle}>
+                  <img src={first} className="h-44 w-44 rounded-full border-2"></img>
+                  <NavDropdown.ItemText className="">{patient.fname}{patient.lname}</NavDropdown.ItemText>
+                  <NavDropdown.ItemText className="">{patient.email}</NavDropdown.ItemText>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item className="text-center"> 
+                    PROFILE
+                  </NavDropdown.Item>
+                  <NavDropdown.Item className="text-center" onClick={handleClick}> 
+                    LOGOUT
+                  </NavDropdown.Item>
+                </NavDropdown>
               </div>
             </div>
           )}
