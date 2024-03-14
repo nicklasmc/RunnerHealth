cf4ad2daf5cad320890ff61442250ff3e2e78bf7
Nicklas Chiang <nicklasmc1@gmail.com>
2024-03-14 16:25:41 -0700

    Minor adjustments to confirmation styling and theme

--- frontend/src/pages/ApptConfirmation.js
+++ frontend/src/pages/ApptConfirmation.js
@@ -18,39 +18,49 @@ const ApptConfirmation = () => {
   return (
     <div>
       <div className='appt-main-container'>
-        <h1 className='subsec-title'>Appointment Request Received!</h1>
-          <div className='confirm-appt-msg'>
-            <h2 className='confirm-appt-msg-cont'> Thank you {appt.patientFirstName}, 
-              your appointment has been successfully scheduled! A member of our team 
-              will reach out to you soon. Attached below is all the information needed
-              for your upcoming appointment. Please come prepared and on time for your
-              appointment.<strong><em> If you wish to cancel or reschedule your 
-                apppointment, please contact us as soon as possible.</em></strong></h2>
-          </div>
-        <h2 className='confirm-appt-table'> 
-          <strong>Appointment information for {appt.patientFirstName} {appt.patientLastName}</strong></h2>
-            <table className='confirm-appt-table'>
-              <tbody>
-                <tr>
-                  <td> <strong>Your {appt.apptReason} appointment in regards to:</strong> </td>
-                  <td> <strong>Any additional information regarding your appointment with: ~Dr.~
-                    will be sent to your provided contact information:</strong></td>
-                </tr>
-              </tbody>  
-              <tbody> 
-                <tr>
-                  <td> {appt.apptComments} is scheduled for {appt.preferredDate} at {appt.time}</td>
-                  <td>Email: {appt.patientEmail} Phone: {appt.patientPhone} </td>
-                </tr>
-              </tbody>
-            </table>
-        <h2 className='notice'> Please save this message in the event that you need to
-          reschedule or cancel your appointment</h2>
-        <Link to="/patient_home" className="link-btn" >
-          <p className='appt-main-container-btn'>
-            Click here to return Home
-          </p>
-        </Link>
+        <div className='appt-container'>
+          <h1 className='confirm-title'>Appointment Request Received!</h1>
+            <div className='confirm-appt-msg'>
+              <h2 className='confirm-appt-msg-cont'> Thank you {appt.patientFirstName}, 
+                your appointment has been successfully scheduled!</h2>
+                <br/>
+                <br/>
+                <h2 className='confirm-appt-msg-cont'>A member of our team 
+                will reach out to you soon. Please come prepared and on time for your
+                appointment.</h2>
+              <h2 className='confirm-appt-msg-notice'><strong><em> If you wish to cancel or reschedule, 
+                please contact your provider as soon as possible.</em></strong></h2>
+            </div>
+          <h2 className='confirm-appt-table'> 
+            <strong>Appointment information for {appt.patientFirstName} {appt.patientLastName}</strong></h2>
+              <table className='confirm-appt-table'>
+                <tbody>
+                  <tr>
+                    <td> <strong>Your {appt.apptReason} appointment is scheduled for:</strong> </td>
+                    <td> <strong>Additional information regarding your appointment will be sent to your contact information:</strong></td>
+                  </tr>
+                </tbody>  
+                <tbody> 
+                  <tr>
+                    <td> {appt.preferredDate}</td>
+                    <td>Email: {appt.patientEmail}</td>
+                  </tr>
+                </tbody>
+                <tbody> 
+                  <tr>
+                    <td>at {appt.time}</td>
+                    <td>Phone: {appt.patientPhone} </td>
+                  </tr>
+                </tbody>
+              </table>
+          <h2 className='notice'> Please save this confirmation in the event that you need to
+            reschedule or cancel your appointment.</h2>
+          <Link to="/patient_home" className="appt-return">
+            <button className="appt-main-container-btn">
+              Click here to return Home
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
