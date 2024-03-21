3c36e593fd8ee5364f9190687e277d3aad9f8cfe
nromasanta <nichomas14@gmail.com>
2024-03-21 16:31:01 -0700

    Simplified confirmation logic

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -47,25 +47,24 @@ const getPatientAppointment = async (req, res) => {
     return res.status(404).json({ error: "Patient does not exist" });
   }
   try {
-    const appointment = await Appointment.find({patientId: id});
+    const appointment = await Appointment.find({ patientId: id });
     res.status(200).json(appointment);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
-const updateApptStatus = async (req,res) => {
-  const {id} = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({error: "Appointment does not exist"})
-  }
+const updateApptStatus = async (req, res) => {
+  const { id } = req.params;
+  // if (!mongoose.Types.ObjectId.isValid(id)) {
+  //   return res.status(400).json({error: "Appointment does not exist"})
+  // }
   try {
     const appointment = await Appointment.findByIdAndUpdate(id, req.body);
     res.status(200).json(appointment);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
-
 };
 // get appointment populated with doctor's information as well
 const getApptAndDoctor = async (req, res) => {
@@ -82,54 +81,52 @@ const removeDate = async (req, res) => {
   try {
     const appts = await ApptDate.deleteOne({ appointment: id });
     res.status(200).json(appts);
-  
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
 
-const getTakenDates = async (req, res) => {
-  const { id } = req.params; // id corresponds to doctor's mongoDB ID
+const getApptsByDate = async (req, res) => {
+  const { date } = req.params;
+  const { id } = req.params;
   try {
-    // significantly more complex query, deeper search
-    // time to use mongodb functions... great..
+    // populate, but only if it matches the doctor's id
+    const appts = await ApptDate.find({ apptDay: date })
+      .populate("time09")
+      .populate({
+        path: "time09",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time10",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time11",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time12",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time13",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time14",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time15",
+        match: { doctor: id },
+      })
+      .populate({
+        path: "time16",
+        match: { doctor: id },
+      });
 
-    // steps:
-    // 1. get appointments belonging to a doctor
-    // 2. cross-reference with the takenDates in apptDates collection
-    // 3. extract and return only the takenDates
-
-    const gatheredDates = await Appointment.aggregate([
-      // 1. get appointments belonging to a doctor, ensure of type mongoose id
-      { $match: { doctor: new mongoose.Types.ObjectId(id) } },
-
-      // 2. cross-reference with the takenDates in apptDates collection
-      {
-        // still in appointments,
-        $lookup: {
-          from: "apptdates", // inside apptdates
-          localField: "_id", // still in appointments, use the id's per doc
-          foreignField: "appointment", // do not confuse for collection! this is a FIELD
-          as: "apptDates", // store as apptdates
-        },
-      },
-
-      { $unwind: "$apptDates" }, // apptDates got from the 'as' field above
-
-      // 3. extract and return only the takenDates
-      {
-        $project: {
-          _id: 0, // 0 to disable, don't need this here
-          takenDate: "$apptDates.takenDate", // project all the takenDates
-        },
-      },
-
-      // reached the final stage, save into gatheredDates variable
-    ]);
-
-    const takenDates = gatheredDates;
-    console.log(takenDates);
-    res.status(200).json(takenDates);
+    res.status(200).json(appts);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -153,12 +150,11 @@ const updateAppointment = async (req, res) => {
 const checkDate = async (req, res) => {
   const { date } = req.params;
   try {
-    const day = await ApptDate.findOne({apptDay: new Date(date)});
+    const day = await ApptDate.findOne({ apptDay: new Date(date) });
     if (day) {
-      res.json({found:true});
-    }
-    else {
-      res.json({found:false});
+      res.json({ found: true });
+    } else {
+      res.json({ found: false });
     }
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -169,7 +165,9 @@ const createApptDate = async (req, res) => {
   const { preferredDate } = req.params;
   console.log(preferredDate);
   try {
-    const newApptDate = await ApptDate.create({ apptDay: new Date(preferredDate) });
+    const newApptDate = await ApptDate.create({
+      apptDay: new Date(preferredDate),
+    });
     res.status(200).json(newApptDate);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -177,63 +175,72 @@ const createApptDate = async (req, res) => {
 };
 
 const createConfirmedAppt = async (req, res) => {
-  const { apptDay, timeSlot, appointmentId } = req.body;
-  console.log("Timeslot -----> ",timeSlot);
+  const { apptDay, timeSlot, appointmentId, docId } = req.body;
+  console.log("Timeslot -----> ", timeSlot);
+  console.log("Day -----> ", apptDay);
+  console.log("ID -----> ", appointmentId);
+  console.log("Doc -----> ", docId);
+
   try {
-    const day = new Date(apptDay);
-    const apptDate = await ApptDate.findOne({ apptDay: day });
-    // useful ref https://www.mongodb.com/docs/manual/reference/operator/query/in/
-    if (apptDate && apptDate[timeSlot]) {
-      // appointment is sitting in the appt collection, grab it w/ doctor id
-      const bookingAppt = await Appointment.findById(appointmentId);
-      const doctorId = bookingAppt.doctor;
-
-      // take everything in the appointment collection
-      // 1. find inside appt collection
-      // 2. break it down to where the _id is equal to...
-      // 3. ... the appt id IN the timeslot (because we only need THOSE)
-      // 4. now that we have it, populate existingappt w/ doctor info
-      // 5. appointments are now populated w/ doctor info
-      const existingAppointments = await Appointment.find({
-        _id: { $in: apptDate[timeSlot] },
-      }).populate("doctor");
-
-      // iterate through every single appointment, find if the doctor
-      // is booked or not
-      for (let i = 0; i < existingAppointments.length; i++) { // length of arr.;
-        console.log(`Comparing ${existingAppointments[i].doctor._id} with ${doctorId}`);
-        if (existingAppointments[i].doctor._id.equals(doctorId)) {
-          console.log("Doctor Already booked...");
-          return res.status(200).json({ message: "false" });
-        }
-      }
+    // 1: grab the date, populate it
+    let apptDate = await ApptDate.findOne({ apptDay: apptDay }).populate({
+      path: timeSlot,
+      populate: {
+        path: "doctor",
+      },
+    });
+    if (!apptDate) {
+      console.log("No apptDate found");
+      const newApptDate = await ApptDate.create({
+        apptDay: new Date(apptDay),
+      });
+      console.log("Created new ApptDate:", newApptDate);
+      apptDate = await ApptDate.findOne({ apptDay: apptDay }).populate({
+        path: timeSlot,
+        populate: {
+          path: "doctor",
+        },
+      });
     }
 
-    if (!apptDate) { 
-      console.log("ApptDate not found!!!");
-      // create apptDate if we need it
-      const newApptDateData = {
-        apptDay: day,
-        [timeSlot]: [appointmentId]
-      };
-      console.log("checkpoint waiting... new apptdate");
-      await ApptDate.create(newApptDateData);
-      console.log("checkpoint created new apptdate");
-    } else {
-      // push into the timeslot
-      console.log("ApptDate found, updating...");
+    let currentDocs = [];
+    // 2. gather all doctors in the timeslot
+    apptDate[timeSlot].forEach((element) => {
+      currentDocs.push(element.doctor._id.toString()); // conv. to string for processing
+    });
+    var filteredDocs = currentDocs.filter((val) => val === docId);
+    console.log("currentDocs:", currentDocs);
+    console.log("filteredDocs:", filteredDocs);
+
+    // check timeslot against what we have
+    if (filteredDocs.length === 0) {
+      console.log("timeslot is empty!");
       apptDate[timeSlot].push(appointmentId);
       await apptDate.save();
+      res.status(200).json({ message: "true" });
+    } else {
+      console.log("timeslot is reserved, push denied");
+      return res.status(200).json({ message: "false" });
     }
-
-    console.log("checkpoint final");
-    res.status(200).json({ message: "true" });
   } catch (error) {
-    console.error("Error creating confirmed appointment:", error);
-    res.status(400).json({ message: error.message });
+    return res.status(400).json({ message: "false" });
   }
 };
 
+// gets all appt's listed under doctor's id
+const getDoctorAppointment = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Doctor does not exist" });
+  }
+  try {
+    const appointment = await Appointment.find({ doctor: id });
+    // console.log(appointment);
+    res.status(200).json(appointment);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 module.exports = {
   updateAppointment,
@@ -243,9 +250,10 @@ module.exports = {
   getAllAppointments,
   getPatientAppointment,
   getApptAndDoctor,
-  getTakenDates,
+  getApptsByDate,
   removeDate,
   updateApptStatus,
   checkDate,
-  createApptDate
+  createApptDate,
+  getDoctorAppointment,
 };
