986062219c5d5d88232cfe707e8f7af6063ec4e3
Nicklas Chiang <nicklasmc1@gmail.com>
2024-04-10 21:38:51 -0700

    Landing page v2.0 with mobile styling

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { FaGithubSquare, FaLinkedin, FaPortrait } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { VscCalendar, VscKey, VscFolder } from 'react-icons/vsc';
 
 /*images*/
 import first from './imgs/doctors-patient.jpg';
@@ -24,67 +26,117 @@ import fourteen from './imgs/healthcare-worker.jpg';
 import fifteen from './imgs/patient.jpg';
 
 function Landing() {
+  const isBreakpoint = useMediaQuery(800)
+
   return (
-    <div className="page-contents landing-page">
-      <Carousel className="landing-showcase">
-        <Carousel.Item>
-          <img src={first} className="showcase-img" alt="..." />
-          <Carousel.Caption>
-            <h3 className="carousel-header">Better Healthcare</h3>
-            <p>Runner Health helps to improve the quality of healthcare.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={second} className="showcase-img" alt="..." />
-          <Carousel.Caption>
-            <h3 className="carousel-header">Electronic Health Records</h3>
-            <p>
-              Store medical records more efficiently by utilizing the cloud.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={third} className="showcase-img" alt="..." />
-          <Carousel.Caption>
-            <h3 className="carousel-header">Faster Response Times</h3>
-            <p>
-              Improve response times with EHRs and automating administrative
-              tasks.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div className="landing-page">
+      <div className="landing-showcase">
+        <Carousel>
+          <Carousel.Item>
+            <img src={first} className="showcase-img" alt="..." />
+            <Carousel.Caption>
+              <h3 className="carousel-header">Better Healthcare</h3>
+              <p>Runner Health helps to improve the quality of healthcare.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={second} className="showcase-img" alt="..." />
+            <Carousel.Caption>
+              <h3 className="carousel-header">Electronic Health Records</h3>
+              <p>
+                Store medical records more efficiently by utilizing the cloud.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={third} className="showcase-img" alt="..." />
+            <Carousel.Caption>
+              <h3 className="carousel-header">Faster Response Times</h3>
+              <p>
+                Improve response times with EHRs and automating administrative
+                tasks.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
       <div className="rh-landing-bit">
-        <div className="runner-what container-fluid">
-          <div className="runner-what-l">
-            <h1 className="subsec-title">TIRED OF WAITING?</h1>
-            <h2 className="subsec-title2">Schedule, Manage, Store, Recieve, Runner
-              Health is a service that does it all</h2>
-              <p className="runner-overflow">
-                Make Healthcare easy with Runner Health. We know Doctor's 
-                visits can be frustrating, The team at Runner Health is
+        { isBreakpoint ? (
+          <div className="runner-hook-small">
+            <div className="runner-hook-small-a">
+              <VscCalendar color="goldenrod" fontSize="1.0em"/>
+              <p className="runner-hook-small-sec">Schedule Appointments</p>
+            </div>
+            <div className="runner-hook-small-b">
+              <VscFolder color="goldenrod" fontSize="1.0em"/>
+              <p className="runner-hook-small-sec">Store Medical Records</p>
+            </div>
+            <div className="runner-hook-small-c">
+              <VscKey color="goldenrod" fontSize="1.0em"/>
+              <p className="runner-hook-small-sec">Seamless Payment</p>
+            </div>
+          </div>
+        ) : (
+          <div className="runner-hook">
+            <p className="runner-hook-header">
+              Explore Our Services
+            </p>
+            <div className="runner-hook-text">
+              <div className="runner-hook-a">
+                <p className="runner-hook-sec-text">Schedule Appointments</p>
+                <p className="runner-hook-a-text">
+                  Setup healthcare appointments with a provider
+                </p>
+              </div>
+              <p className="runner-hook-slant">/</p>
+              <div className="runner-hook-b">
+                <p className="runner-hook-sec-text">Store Medical Records</p>
+                <p className="runner-hook-a-text">
+                  Upload and retrieve medical health records
+                </p>
+              </div>
+              <p className="runner-hook-slant">/</p>
+              <div className="runner-hook-c">
+                <p className="runner-hook-sec-text">Seamless Payment</p>
+                <p className="runner-hook-a-text">
+                  Pay and review invoices from appointments
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="runner-what">
+          <div className="runner-what-content">
+            <div className="runner-what-a">
+              <p className="runner-what-title">TIRED OF WAITING?</p>
+              <div className="runner-what-text">
+                <p className="runner-what-hook">
+                  Make Healthcare easy with Runner Health. 
+                </p>
+                <p className="runner-what-paragraph">
+                We know Doctor's visits can be frustrating. The team at Runner Health is
                 dedicated to providing an easy to use Electronic Health 
                 Record service that works with your providers to safely
                 and securely store your information. Access your health
                 records and schedule your appointments with Runner Health
                 Today!
-              </p>
-            <p className="runner-overflow-b">
+                </p>
+              </div>
               <Link to="/patient_login" className="appt-return">
                 <button className="appt-button">
                   Explore with us
                 </button>
               </Link>
-            </p>
-          </div>
-          <div className="runner-what-r">
-            <img src={fourth} className="runner-img" />
+            </div>
+            <div className="runner-what-b">
+              <img src={fourth} className="runner-what-img" />
+            </div>
           </div>
         </div>
        
         <div className="runner-team">
           <h1 className="subsec-title">The Runner Health Team</h1>
-          <div className="runner-team-box container-fluid">
+          <div className="runner-team-box">
             <Card className="team-member-card">
               <Card.Img variant="top" className="team-img" src={seven} />
               <Card.Body>
@@ -229,5 +281,31 @@ function Landing() {
     </div>
   );
 }
+
+
+const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addEventListener("change", updateTarget);
+
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeEventListener("change", updateTarget);
+  }, []);
+
+  return targetReached;
+};
 
 export default Landing;
